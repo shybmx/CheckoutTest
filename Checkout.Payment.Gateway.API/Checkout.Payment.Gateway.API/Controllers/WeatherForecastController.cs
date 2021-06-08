76cb53b8a6dfba23984ba91bc8333a537414a5b9
Shahzad Emambaccus <shay_619@hotmail.co.uk>
2021-06-08 12:44:08 +0100

    Init

---
+++ Checkout.Payment.Gateway.API/Checkout.Payment.Gateway.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Checkout.Payment.Gateway.API.Controllers
+{
+    [ApiController]
+    [Route("Payment")]
+    public class WeatherForecastController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return null;
+        }
+    }
+}
