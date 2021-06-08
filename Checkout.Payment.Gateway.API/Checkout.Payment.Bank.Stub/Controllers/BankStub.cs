a88eb5d39726bb4f92063d31065582a12b3f3231
Shahzad Emambaccus <shay_619@hotmail.co.uk>
2021-06-08 15:54:20 +0100

    Cosmos DB for stub

---
+++ Checkout.Payment.Gateway.API/Checkout.Payment.Bank.Stub/Controllers/BankStub.cs
@@ -0,0 +1,37 @@
+﻿using Checkout.Payment.Gateway.Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using System;
+using System.Threading.Tasks;
+
+namespace Checkout.Payment.Bank.Stub.Controllers
+{
+    [ApiController]
+    [Route("Banking")]
+    public class BankStub : ControllerBase
+    {
+        private static readonly string EndpointUri = "https://checkoutshaytest.documents.azure.com:443/";
+        private static readonly string PrimaryKey = "zyjUyKuyxHtrWz3YIyBM9Ba02txAJikbatlxUUZNGqGGwUJURZKwzUu3Lj8VtjEKmAGSfkS8myG7P9wFCPRVXg==";
+        private CosmosClient _cosmosClient;
+        private Database _database;
+        private Container _container;
+
+        [HttpGet]
+        public async Task<IActionResult> BankExecutePurchase([FromQuery] PaymentDetails paymentDetails)
+        {
+            _cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
+            _database = await _cosmosClient.CreateDatabaseIfNotExistsAsync("bank");
+            _container = await _database.CreateContainerAsync("bankConatiner", "/partition");
+
+            try
+            {
+                var paymentToUpload = await _container.CreateItemAsync<PaymentDetails>(paymentDetails, new PartitionKey(paymentDetails.Currency));
+                return new OkObjectResult(new BankResponse { Identifier = new Guid(), PaymentSuccessful = true });
+            }
+            catch(Exception e)
+            {
+                return new OkObjectResult(new BankResponse { PaymentSuccessful = false });
+            }
+        }
+    }
+}
--- Checkout.Payment.Gateway.API/Checkout.Payment.Gatway.Contracts/PaymentDetails.cs
+++ Checkout.Payment.Gateway.API/Checkout.Payment.Gatway.Contracts/PaymentDetails.cs
@@ -4,10 +4,13 @@ namespace Checkout.Payment.Gateway.Contracts
 {
     public class PaymentDetails
     {
+        //This Id is for cosmos DB
+        public string id => Identifier.ToString();
         public long CardNumber { get; set; }
         public DateTime Expiry { get; set; }
         public double Amount { get; set; }
         public string Currency { get; set; }
         public int Cvv { get; set; }
+        public Guid Identifier => new Guid();
     }
 }
