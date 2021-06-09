6b017eca3bf9010c3f3e567d9b91e07e9f3e0f3f
Shahzad Emambaccus <shay_619@hotmail.co.uk>
2021-06-09 14:36:51 +0100

    Update bank stub

--- Checkout.Payment.Gateway.API/Checkout.Payment.Bank.Stub/Controllers/BankStub.cs
+++ Checkout.Payment.Gateway.API/Checkout.Payment.Bank.Stub/Controllers/BankStub.cs
@@ -1,6 +1,5 @@
 ﻿using Checkout.Payment.Gateway.Contracts;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Azure.Cosmos;
 using System;
 using System.Threading.Tasks;
 
@@ -10,62 +9,18 @@ namespace Checkout.Payment.Bank.Stub.Controllers
     [Route("Banking")]
     public class BankStub : ControllerBase
     {
-        private static readonly string EndpointUri = "https://checkoutshaytest.documents.azure.com:443/";
-        private static readonly string PrimaryKey = "zyjUyKuyxHtrWz3YIyBM9Ba02txAJikbatlxUUZNGqGGwUJURZKwzUu3Lj8VtjEKmAGSfkS8myG7P9wFCPRVXg==";
-        private CosmosClient _cosmosClient;
-        private Database _database;
-        private Container _container;
-
         [HttpPost("executePurchase")]
         public async Task<IActionResult> BankExecutePurchase([FromBody] PaymentDetails paymentDetails)
         {
-            await CreateCosmosConnection();
+            var isSuccessful = false;
 
-            try
+            var randomNumber = new Random();
+            if(randomNumber.Next(0, 100) % 0 == 0)
             {
-                paymentDetails.Identifier = Guid.NewGuid();
-                var paymentToUpload = await _container.CreateItemAsync<PaymentDetails>(paymentDetails, new PartitionKey(paymentDetails.Currency));
-                return new OkObjectResult(new BankResponse { Identifier = paymentDetails.Identifier, PaymentSuccessful = true });
-            }
-            catch(Exception e)
-            {
-                return new OkObjectResult(new BankResponse { PaymentSuccessful = false });
-            }
-        }
-
-        [HttpPost("getDetails")]
-        public async Task<IActionResult> GetPurchaseDetails([FromBody] Guid paymentGuid)
-        {
-            await CreateCosmosConnection();
-
-            var sqlQueryText = $"SELECT * FROM c WHERE c.id = '{paymentGuid}'";
-
-            try
-            {
-                var queryDefinition = new QueryDefinition(sqlQueryText);
-                var queryResultSetIterator = _container.GetItemQueryIterator<PaymentDetails>(queryDefinition);
-                while (queryResultSetIterator.HasMoreResults)
-                {
-                    var items = await queryResultSetIterator.ReadNextAsync();
-                    foreach (var item in items)
-                    {
-                        return new OkObjectResult(item);
-                    }
-                }
-            }
-            catch(Exception e)
-            {
-                return new NotFoundObjectResult("Payment Not Found");
+                isSuccessful = true;
             }
 
-            return new NotFoundObjectResult("Payment Not Found");
-        }
-
-        private async Task CreateCosmosConnection()
-        {
-            _cosmosClient = new CosmosClient(EndpointUri, PrimaryKey);
-            _database = await _cosmosClient.CreateDatabaseIfNotExistsAsync("bank");
-            _container = await _database.CreateContainerIfNotExistsAsync("bankConatiner", "/Currency");
+            return new OkObjectResult(new BankResponse { Identifier = paymentDetails.Identifier, PaymentSuccessful = isSuccessful });
         }
     }
 }
